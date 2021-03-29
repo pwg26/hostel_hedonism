49f2a218471482321ccd29f84622ad848a9dc1e5
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-29 11:11:58 -0600

    added blank user schema

--- models/User.js
+++ models/User.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs");
+
+const userSchema = new Schema({});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
