6c1941808c1bdc02c7286df7c916fc4e34f0c84e
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-29 16:32:05 -0600

    fixed footer warning

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,29 +1,22 @@
-import React from "react"
-import "./index.js"
-
+import React from "react";
+import "./index.js";
 
 const footerStyle = {
   color: "red",
   fontSize: "20px",
-  textAlign: "center"
-}
+  textAlign: "center",
+};
 const Footer = () => {
-    return (
-      <footer className="footer d-flex flex-column justify-content-center align-items-center bg-dark">
-    
-        <p style={footerStyle} className="lead my-3 text-white">
-          &copy; Made with <span>♥️</span> by{" "}
-          <a
-            id="myInfo"
-            href=""
-            target="_blank"
-            rel="noreferrer"
-          >
-            Kristen, Pierce, Charles, and Dante 
-          </a>
-        </p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <footer className="footer d-flex flex-column justify-content-center align-items-center bg-dark">
+      <p style={footerStyle} className="lead my-3 text-white">
+        &copy; Made with <span>♥️</span> by{" "}
+        <a id="myInfo" href="/" target="_blank" rel="noreferrer">
+          Kristen, Pierce, Charles, and Dante
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
