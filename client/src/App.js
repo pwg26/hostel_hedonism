c4396a4903d49017b04a10363de061c8ea0a5058
Pwg21 <kcaskade@gmail.com>
2021-03-29 13:58:02 -0400

    updated file structure, added pages folder, components folder, server

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from "./components/Nav";
-import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Navbar from "./components/Nav";
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/search" component={Search} />
+        </Wrapper>
+        <Footer />
       </div>
     </Router>
   );
