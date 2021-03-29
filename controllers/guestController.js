523d2d74f091174cb200418dc3d049d652733bec
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-29 16:22:29 -0600

    added control functions

--- controllers/guestController.js
+++ controllers/guestController.js
@@ -7,4 +7,14 @@ module.exports = {
       res.json(user.comparePassword(password))
     );
   },
+  getRooms: function (req, res) {
+    db.Room.find({}).then((data) => res.json(data));
+  },
+  findGuest: function (req, res) {
+    db.Guest.findOne(req.body)
+      .populate({ path: "reservations", populate: { path: "room" } })
+
+      .populate("activities")
+      .then((guest) => console.log(guest));
+  },
 };
