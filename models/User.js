470ecbc723be0d23985323279d2be39f139ce9c4
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-29 11:31:14 -0600

    added keys to user model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs");
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 32,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 
