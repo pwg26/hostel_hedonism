d442fae2355149b846d3f659214219bdbafcd1ab
Dante James <DanteJames94@yahoo.com>
2021-03-29 12:46:09 -0600

    footer update

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,17 @@
 import React from "react"
+import "./index.js"
 
+
+const footerStyle = {
+  color: "red",
+  fontSize: "20px",
+  textAlign: "center"
+}
 const Footer = () => {
     return (
-      <footer className="d-flex flex-column justify-content-center align-items-center bg-dark">
+      <footer className="footer d-flex flex-column justify-content-center align-items-center bg-dark">
     
-        <p className="lead my-3 text-white">
+        <p style={footerStyle} className="lead my-3 text-white">
           &copy; Made with <span>♥️</span> by{" "}
           <a
             id="myInfo"
