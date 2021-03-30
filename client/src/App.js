b4cbc647102a9491b1e59eec0417dfad302f2bfb
Pwg21 <kcaskade@gmail.com>
2021-03-29 21:49:56 -0400

    added sample table component to be modified and taylored

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/NavBar";
+import BasicTable from "./components/GuestTable";
 // import DashBooard from "./pages/Dashboard";
 // import Guests from "./pages/Guests";
 // import Rooms from "./pages/Rooms";
@@ -12,6 +13,8 @@ function App() {
     <Router>
       <div>
         <Navbar />
+        <BasicTable />
+
         {/* <Wrapper>
           <Route exact path="/" pages={DashBoard} />
           <Route exact path="/dashboard" pages={DashBoard} />
