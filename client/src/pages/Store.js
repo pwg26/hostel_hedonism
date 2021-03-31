4b323e94f64322d49cd68d0182dbda5c1fd666da
Dante James <DanteJames94@yahoo.com>
2021-03-31 11:37:55 -0600

    store table

--- client/src/pages/Store.js
+++ client/src/pages/Store.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 import AddBoxIcon from '@material-ui/icons/AddBox';
 import UpdateIcon from '@material-ui/icons/Update';
+
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -12,8 +20,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function IconLabelButtons() {
-  const classes = useStyles();
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+  
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  const rows = [
+    createData(1, "White Claw", 15.99, "Yes"),
+    
+  ];
+  
+
+  
+  export default function CustomizedTables() {
+    const classes = useStyles();
+
+// export default function IconLabelButtons() {
+//   const classes = useStyles();
 
   return (
     <>
@@ -49,12 +89,50 @@ export default function IconLabelButtons() {
     
     </div>
 
+
+
+
+
+
+
+
+
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell> ID </StyledTableCell>
+            <StyledTableCell align="right">Name</StyledTableCell>
+            <StyledTableCell align="right">Cost&nbsp;</StyledTableCell>
+            <StyledTableCell align="right">Purchased&nbsp;</StyledTableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+              <StyledTableCell align="right">{row.fat}</StyledTableCell>
+              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+              
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+
+
+
     <div>
         
 
 
     </div>
+</>
 
-    </>
   );
-}
+};
