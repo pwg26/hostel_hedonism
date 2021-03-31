781466fde46f1b909d378049809da10d31de975a
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-31 17:23:40 -0600

    fixed a warning

--- client/src/components/GuestModal/index.js
+++ client/src/components/GuestModal/index.js
@@ -6,7 +6,7 @@ import Button from "@material-ui/core/Button";
 import API from "../../utils/API";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
-import FormHelperText from "@material-ui/core/FormHelperText";
+
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 
