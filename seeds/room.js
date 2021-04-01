d1b8a38d4089b4de7742ffca6ef31726cd5f2a1d
Pwg21 <kcaskade@gmail.com>
2021-04-01 15:58:17 -0400

    populating cards

---
+++ seeds/room.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project3", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+console.log("Database connected");
+
+const room = {
+  number: 1,
+  name: "Love Shack",
+  rate: 100,
+  capacity: 2,
+  occupants: 2,
+};
+
+db.Room.create(room);
