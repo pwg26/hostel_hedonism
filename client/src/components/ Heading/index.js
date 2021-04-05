619c295b0a7cade70b3da0ea5201a8737111c9d0
Dante James <DanteJames94@yahoo.com>
2021-04-05 14:47:19 -0600

    heading style

--- client/src/components/ Heading/index.js
+++ client/src/components/ Heading/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import API from "../../utils/API";
 export default function Heading(props) {
-  return <h1> {props.heading}</h1>;
+ 
+
+
+  return <h1 style={{fontFamliy: "Italic"}}> {props.heading}</h1>;
 }
