1d0d0e1dd693cc9b9fa9737ed4be1cd60dd2363a
Kristen Campbell <kristenc1031@gmail.com>
2021-04-05 11:18:39 -0600

    changed footer color

--- seeds/room.js
+++ seeds/room.js
@@ -9,12 +9,34 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project3", {
 
 console.log("Database connected");
 
-const room = {
-  number: 1,
-  name: "Love Shack",
-  rate: 100,
-  capacity: 2,
-  occupants: 2,
-};
-
+const room = [
+  {
+    number: 1,
+    name: "Love Shack",
+    rate: 100,
+    capacity: 2,
+    occupants: 2,
+  },
+  {
+    number: 2,
+    name: "Moon Room",
+    rate: 100,
+    capacity: 2,
+    occupants: 2,
+  },
+  {
+    number: 3,
+    name: "Sun Room",
+    rate: 100,
+    capacity: 2,
+    occupants: 2,
+  },
+  {
+    number: 4,
+    name: "Suite",
+    rate: 100,
+    capacity: 4,
+    occupants: 4,
+  },
+];
 db.Room.create(room);
