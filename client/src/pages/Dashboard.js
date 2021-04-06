fb8bb67464e9d965a761a8ef6c33871fa4220e33
Charles Zoeller <charles.zoeller1@gmail.com>
2021-04-05 20:58:31 -0600

    added income chart

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -7,10 +7,56 @@ import DashGuest from "../components/DashGuest";
 // import DashPro from "../components/DashPro";
 import API from "../utils/API";
 
+import Paper from "@material-ui/core/Paper";
+import {
+  Chart,
+  PieSeries,
+  Title,
+  Legend,
+} from "@devexpress/dx-react-chart-material-ui";
+
+import { Animation, EventTracker } from "@devexpress/dx-react-chart";
+
+class TestChart extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      info: props["guests"],
+      targetItem: undefined,
+    };
+  }
+
+  render() {
+    const { info: chartData, targetItem } = this.state;
+    const data = this.props.guests;
+    console.log(chartData, data, targetItem);
+
+    return (
+      <Paper
+        style={{
+          width: "50%",
+          marginLeft: "auto",
+          marginRight: "auto",
+          float: "right",
+        }}
+      >
+        <Chart data={data}>
+          <PieSeries valueField="val" argumentField="type" innerRadius={0.6} />
+          <Title text="Total incom Breakdown" />
+          <Animation />
+          <Legend />
+        </Chart>
+      </Paper>
+    );
+  }
+}
+
 export default function Dash() {
   const [activities, setActivities] = useState([]);
   const [filtered, setFiltered] = useState([]);
   const [guests, setGuests] = useState([]);
+  const [chartData, setChartData] = useState([{}]);
 
   useEffect(() => {
     const loadActivities = () => {
@@ -49,16 +95,17 @@ export default function Dash() {
             const dayOut = new Date(guest.reservation.checkOut);
             const duration = Math.floor((dayOut - dayIn) / 8.64e7);
             //console.log(duration);
-            const activities = guest.activities.reduce(
+            const costA = guest.activities.reduce(
               (sum, curr) => sum + curr.cost,
               0
             );
+
             const rent = duration * guest.reservation.room.rate;
 
-            // const Cart= guest.shoppingCart.reduce(
-            //   (sum, curr) => sum + curr.cost,
-            //   0
-            // );
+            const costS = guest.purchases.reduce(
+              (sum, curr) => sum + curr.cost,
+              0
+            );
             //console.log(guest.reservation.room, guest.reservation.room.rate);
             return {
               firstName: guest.firstName,
@@ -70,48 +117,12 @@ export default function Dash() {
               duration: duration,
               paid: guest.paid ? "Yes" : "No",
               checkedIn: guest.checkedIn ? "Yes" : "No",
-              activities: activities,
+              activities: guest.activities,
+              purchases: guest.purchases,
               rent: rent,
-              tab: `$ ${activities + rent}`,
-              room: guest.reservation.room.name,
-              roomId: guest.reservation.room._id,
-            };
-          })
-        );
-        setFiltered(
-          res.data.map((guest) => {
-            const dayIn = new Date(guest.reservation.checkIn);
-            const dayOut = new Date(guest.reservation.checkOut);
-            const duration = Math.floor((dayOut - dayIn) / 8.64e7);
-            //console.log(duration);
-            const activities = guest.activities.reduce(
-              (sum, curr) => sum + curr.cost,
-              0
-            );
-            const purchases = guest.purchases.reduce(
-              (sum, curr) => sum + curr.cost,
-              0
-            );
-            const rent = duration * guest.reservation.room.rate;
-
-            // const Cart= guest.shoppingCart.reduce(
-            //   (sum, curr) => sum + curr.cost,
-            //   0
-            // );
-            //console.log(guest.reservation.room, guest.reservation.room.rate);
-            return {
-              firstName: guest.firstName,
-              lastName: guest.lastName,
-              id: guest._id,
-              country: guest.country,
-              dateIn: dayIn.toDateString(),
-              dateOut: dayOut.toDateString(),
-              duration: duration,
-              paid: guest.paid ? "Yes" : "No",
-              checkedIn: guest.checkedIn ? "Yes" : "No",
-              activities: activities,
-              rent: rent,
-              tab: `$ ${activities + rent}`,
+              tab: `$ ${costA + rent + costS}`,
+              costA: costA,
+              costS: costS,
               room: guest.reservation.room.name,
               roomId: guest.reservation.room._id,
             };
@@ -122,9 +133,22 @@ export default function Dash() {
     loadGuests();
   }, []);
 
+  useEffect(() => {
+    const act = guests.reduce((prev, curr) => (prev += curr.costA), 0);
+    const store = guests.reduce((prev, curr) => (prev += curr.costS), 0);
+    const rent = guests.reduce((prev, curr) => (prev += curr.rent), 0);
+    console.log(act, store, rent);
+    setChartData([
+      { type: "Activities", val: act },
+      { type: "Store Purchases", val: store },
+      { type: "Rooms", val: rent },
+    ]);
+  }, [guests]);
+
   return (
     <>
       <Heading heading="Dashboard" />
+      <TestChart guests={chartData} />
       <DashAct acts={activities} />
       <DashGuest guests={guests} />
     </>
