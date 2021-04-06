d73fa79b307a6fa51588f5c937afbfec21e7c70b
Charles Zoeller <charles.zoeller1@gmail.com>
2021-04-05 21:01:09 -0600

    readded footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./index.js";
-
-const footerStyle = {
-  color: "black",
-  fontSize: "20px",
+import AppBar from "@material-ui/core/Button";
+var style = {
+  backgroundColor: "#F8F8F8",
+  borderTop: "1px solid #E7E7E7",
   textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "35px",
+  width: "100%",
 };
 const Footer = () => {
   return (
-    <footer className="footer fixed justify-content-center align-items-center bg-dark">
-      <p style={footerStyle}  className="lead my-3 text-white">
-        &copy; Made with <span>♥️</span> by{" "}
-        <a id="myInfo" rel="noreferrer">
-          Kristen, Pierce, Charles, and Dante
-        </a>
-      </p>
-    </footer>
+    <div>
+      <div style={style}>Created by Dante, Kristen, and Pierce</div>
+    </div>
   );
 };
-
 export default Footer;
